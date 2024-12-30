96cce67344426608d4898ffebaa96a3551062729
Bhoasx <165915947+Bhoasx@users.noreply.github.com>
2024-12-30 09:59:27 -0500

    Add files via upload

---
+++ python_desde_cero.py
@@ -0,0 +1,34 @@
+print("¡Hola, Mundo!")
+
+print("La Witsi Witsi Araña\n subió a su telaraña.")
+print("Vino la lluvia\n y se la llevó.")
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\nay se la llevó.")
+
+print("La Witsi Witsi Araña" , "subió" , "a su telaraña.")
+
+print("Mi nombre es", "Python.")
+print("Monty Python.")
+
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+print("Mi nombre es ", end="")
+print("Monty Python.")
+
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("Programming","Essentials","in")
+print("Python")
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
