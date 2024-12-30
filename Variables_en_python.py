8ccb465a1c6ae8870b13d2be137b4bada7066647
Bhoasx <165915947+Bhoasx@users.noreply.github.com>
2024-12-30 10:40:29 -0500

    Add files via upload

---
+++ Variables_en_python.py
@@ -0,0 +1,46 @@
+var = 1
+print(var)
+
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+var = 100
+var = 200 + 300
+print(var)
+
+x = 1
+sheep = 0
+
+x = x * 2
+
+x *= 2
+sheep += 1
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = 1.61
+kilometers_to_miles = 0.62
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
+var = 2
+print(var)
+
+var = 3
+print(var)
+
+var += 1
+print(var)
+
+var = "007"
+print("Agent " + var)
