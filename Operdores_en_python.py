7d68e4f721239c718b80a5b408a25def000b6353
Bhoasx <165915947+Bhoasx@users.noreply.github.com>
2024-12-30 10:24:34 -0500

    Add files via upload

---
+++ Operdores_en_python.py
@@ -0,0 +1,59 @@
+print("2")
+print(2)
+
+print(0o123)
+
+print(0x123)
+
+print("Me gusta \"Monty Python\"")
+
+print('Me gusta "Monty Python"')
+
+print(True > False)
+print(True < False)
+
+print(2+2)
+
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+print(6 // 4)
+print(6. // 4)
+
+print(-6 // 4)
+print(6. // -4)
+
+print(14 % 4)
+
+print(12 % 4.5)
+
+print(-4 + 4)
+print(-4. + 8)
+
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+
+print(+2)
+
+print(9 % 6 % 2)
+
+print(2 * 3 % 5)
+    
