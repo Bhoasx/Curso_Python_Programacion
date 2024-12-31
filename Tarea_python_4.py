866964f9905b58d67a57d004cf55af814a91bec5
Bhoasx <165915947+Bhoasx@users.noreply.github.com>
2024-12-31 10:32:31 -0500

    Add files via upload

---
+++ Tarea_python_4.py
@@ -0,0 +1,8 @@
+entrada = input("Ingresa el nombre de la planta: ")
+
+if entrada == "ESPATIFILIO":
+    print("Si - ¡El Espatifilo! es la mejor planta de todos los tiempos!")
+elif entrada == "espatifilo":
+    print("No, ¡quiero un gran Espatifilo!")
+else:
+    print(f"¡Espatifilo!, ¡No {entrada}!")
---
+++ condiciones_en_python.py
@@ -0,0 +1,52 @@
+if the_weather_is_good:
+    go_for_a_walk()
+have_lunch()
+
+if sheep_counter >= 120: # Evaluar una expresión condicional
+    sleep_and_dream() # Ejecutar si la expresión condicional es verdadera
+
+if sheep_counter >= 120:
+    make_a_bed()
+    take_a_shower()
+    sleep_and_dream()
+feed_the_sheepdogs()
+
+if true_or_false_condition:
+    perform_if_condition_true
+else:
+    perform_if_condition_false
+
+if the_weather_is_good:
+    go_for_a_walk()
+else:
+    go_to_a_theater()
+have_lunch()
+
+if the_weather_is_good:
+    go_for_a_walk()
+    have_fun()
+else:
+    go_to_a_theater()
+    enjoy_the_movie()
+have_lunch()
+
+if the_weather_is_good:
+    if nice_restaurant_is_found:
+        have_lunch()
+    else:
+        eat_a_sandwich()
+else:
+    if tickets_are_available:
+        go_to_the_theater()
+    else:
+        go_shopping()
+
+if the_weather_is_good:
+    go_for_a_walk()
+elif tickets_are_available:
+    go_to_the_theater()
+elif table_is_available:
+    go_for_lunch()
+else:
+    play_chess_at_home()
+
